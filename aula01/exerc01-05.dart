fba590b50f9b1f2d09f712bb2cf3edd6d7fc5f12
Luiz Mateus <103285835+mateusrobr@users.noreply.github.com>
2023-07-26 10:48:09 -0300

    working with folders to first class

---
+++ aula01/exerc01-05.dart
@@ -0,0 +1,70 @@
+class Aluno{
+  String ? _nome;
+  int ? _idade;
+  double ? _peso;
+  Map<dynamic, dynamic>  _disciplinasNotasMap;
+  String ? endereco;
+  bool ? _ehNovato;
+  String ? redacaoFerias;
+  
+  Aluno(this._nome, this._idade, this._peso, this._disciplinasNotasMap, this._ehNovato, {this.redacaoFerias,this.endereco}){
+    
+  }
+  
+  void atribuirDisciplinaNota(String disciplina, double nota){
+    if(_disciplinasNotasMap.length > 4){
+      print("Ja temos 4 disciplinas");
+      return;
+    }
+    _disciplinasNotasMap.addAll({disciplina : nota});
+  
+  }
+  void corrigirErrosRedacao(){
+    redacaoFerias = redacaoFerias?.replaceAll("mais melhor", "melhor");
+    print("Teste");
+  }
+  void atribuirRedacao(String redacaoFerias){
+    if(redacaoFerias.contains("Ferias") && redacaoFerias.split(" ").length > 10){
+      this.redacaoFerias = redacaoFerias;
+      corrigirErrosRedacao();
+      return;
+    }
+    print("Redacao invalida");
+  }
+  void updateNotas(double novaNota, String disciplina){
+    
+    _disciplinasNotasMap[disciplina] = novaNota;
+}
+  void printAluno(){
+    print("Nome: $_nome");
+    print("Idade: $_idade");
+    print("Peso: $_peso");
+    print(endereco ?? "Sem endereco");
+    print("Eh novato: $_ehNovato");
+    print("Disciplinas e suas notas: ");
+    _disciplinasNotasMap.forEach((disciplina,nota) => print("Disciplina: $disciplina e Nota: $nota"));
+    print("Redacao: $redacaoFerias");
+  }
+  
+  Map<dynamic,dynamic> get disciplinasNotas{
+    return this._disciplinasNotasMap;
+  }
+  
+  set peso(double newPeso){
+    _peso = newPeso;
+  }
+}
+void main() {
+  Aluno aluno1 = Aluno("Luiz", 20, 60.00, {}, true);
+  aluno1.atribuirDisciplinaNota("Portugues", 7.0);
+  aluno1.atribuirDisciplinaNota("Matematica", 10.0);
+  aluno1.atribuirDisciplinaNota("Estudados Amazonicos", 1.0);
+  aluno1.atribuirDisciplinaNota("Geografia", 5.0);
+  aluno1.atribuirRedacao("Ferias a a a a a a a a a a a a a a a a a a a a a mais melhor");
+  aluno1.printAluno();
+  
+  for(String disciplina in aluno1.disciplinasNotas.keys){
+        aluno1.updateNotas(10, disciplina);
+    }
+  aluno1.printAluno();
+}

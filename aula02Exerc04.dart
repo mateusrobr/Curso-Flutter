416723a3eda3c725b3f225b777e7843f26dcb9c3
Luiz Mateus <103285835+mateusrobr@users.noreply.github.com>
2023-07-26 10:53:15 -0300

    deleting

--- aula02Exerc04.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-void main(){
-  int idade;
-  print("Digite sua idade: ");
-  idade = int.parse(stdin.readLineSync()!);
-  
-  (idade >=18) ? print("Voce é maior de idadade") : print("Voce é menor de idade");
-}
